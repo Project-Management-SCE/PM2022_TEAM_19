6fc5f73a881e8b20a9984e6a203c7eafd6e45936
AdimAzbarga <adeemaz@ac.sce.ac.il>
2022-05-11 21:55:10 +0300

    Revert "Add files via upload"
    This reverts commit aeb34791c091bbe3e854f4eef959f6fdcd499493.

--- test.js
+++ test.js
@@ -1,1 +1,43 @@
-console.log("asd");
+const app = require('./proj/app')
+const request = require('supertest');
+jest.setTimeout(10000);
+test('Login Page', () => {
+    request(app)
+        .get('/login')
+        .expect(200)
+        .end(function(err, res) {
+        });
+});
+
+test('Sign up', () => {
+    request(app)
+        .get('/signup')
+        .expect(200)
+        .end(function(err, res) {
+        });
+});
+
+test('Home Page', () => {
+    request(app)
+        .get('/homepage')
+        .expect(200)
+        .end(function(err, res) {
+        });
+});
+
+test('about page', () => {
+    request(app)
+        .get('/about')
+        .expect(200)
+        .end(function(err, res) {
+        });
+});
+
+test('Map', () => {
+    request(app)
+        .get('/map')
+        .expect(200)
+        .end(function(err, res) {
+        });
+});
+  
